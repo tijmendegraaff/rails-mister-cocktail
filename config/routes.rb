6b7e2c64b1392525e374380b52e3b7919d3301a2
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-23 12:38:59 +0100

    generate controllers coacktails and doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'doses/index'
+
+  get 'doses/show'
+
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/edit'
+
+  get 'doses/update'
+
+  get 'doses/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
+  get 'cocktails/edit'
+
+  get 'cocktails/update'
+
+  get 'cocktails/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
