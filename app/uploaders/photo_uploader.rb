3b07faddfedc3081a02f30d78507584e0e81d1ce
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-24 13:58:55 +0100

    added styling to cards, modded view cocktail

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,15 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :standard do
+    resize_to_fit 800, 600
+  end
+
+  version :medium do
+    cloudinary_transformation width: 360, height: 175, crop: :thumb, gravity: :face
+  end
 end
